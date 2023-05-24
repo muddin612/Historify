83726623f1a4e342c5f5a0549f7001f885e1e708
muddin <uddinm4@montclair.edu>
2023-05-24 10:48:44 -0400

    3/24/23

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+const clientId = '811cede9e60344be9193ad3ebee82def';
+const redirectUri = 'https://main--stellular-manatee-a99798.netlify.app/';
+
+function authorizeSpotify(){
+
+    var scopes = ['user-read-playback-state','user-read-currently-playing','user-read-recently-played'];
+
+    var authUrl = 'https://accounts.spotify.com/authorize' +
+    '?response_type=token' +
+    '&client_id=' + encodeURIComponent(clientId) +
+    '&scope=' + encodeURIComponent(scopes.join(' ')) +
+    '&redirect_uri=' + encodeURIComponent(redirectUri);
+
+    var loginWindow = window.open(authUrl, 'Login with Spotify', 'width=400,height=600');
+    window.close();
+
+}
+
+function handleCallback(){
+
+    var hash = window.location.hash.substr(1);
+    var params = new URLSearchParams(hash);
+    var accessToken = params.get('access_token');
+
+    window.close();
+
+    console.log('Access Token:', accessToken);
+
+    getListeningHistory(accessToken);
+}
 function getListeningHistory(accessToken) {
     fetch('https://api.spotify.com/v1/me/player/recently-played', {
       headers: {
@@ -35,4 +65,3 @@ function getListeningHistory(accessToken) {
         console.error('Error:', error);
       });
   }
-  
