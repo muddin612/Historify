f929d631f9b9a24ddaeebd9d0c7f2cfa50b1751f
muddin <uddinm4@montclair.edu>
2023-06-20 19:25:03 -0400

    7:24 back

--- spotify_api.js
+++ spotify_api.js
@@ -49,7 +49,7 @@ async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://127.0.0.1:3000/index.html");
+    params.append("redirect_uri", "https://main--stellular-manatee-a99798.netlify.app/");
     params.append("scope", "user-read-private user-read-email user-top-read user-read-recently-played");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -83,7 +83,7 @@ async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://127.0.0.1:3000/index.html");
+  params.append("redirect_uri", "https://main--stellular-manatee-a99798.netlify.app/");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
